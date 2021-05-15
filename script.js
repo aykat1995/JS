5cdf3dab75fa41e28ee9e81aa220ff74e8d57a71
Aikat <54204536+aykat1995@users.noreply.github.com>
2021-05-15 21:33:31 +1100

    2.12 Пишем приложение часть 2

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-let money = prompt("Ваш бюджет на месяц?", "");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", "");
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+    while(isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", "");
+    }
+}
+
+start();
 
 let appData = {
     budget: money,
@@ -7,15 +17,44 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 }
 
-let q1 = prompt("Введите обязательную статью расходов в этом месяце");
-let q2 = prompt("Во сколько обойдется?");
-let q3 = prompt("Введите обязательную статью расходов в этом месяце");
-let q4 = prompt("Во сколько обойдется?");
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", "");
+        let b = +prompt("Во сколько обойдется?", "");
+    
+        if (typeof(a) === "string" && typeof(b) != null && typeof(a) !== null && a.length < 50 && a != "" && b != "") {
+            appData.expenses[a] = b;
+        } else {
+            console.log("Введены некорректные данные");
+            console.log(i);
+            i--;
+        }
+        i++;
+    }    
+}
 
-appData.expenses.q1 = q2;
-appData.expenses.q3 = q4;
+chooseExpenses();
 
-alert("Ваш бюджет на день: " + appData.budget/30);
+appData.moneyPerDay = (appData.budget / 30).toFixed();
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный достаток: " + appData.moneyPerDay);
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний достаток: " + appData.moneyPerDay); 
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий достаток: " + appData.moneyPerDay);
+} else console.log("Произошла ошибка");
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?", ""),
+            percent = +prompt("Под какой процент?", "");
+        appData.mothIncome = save / 100 /12 * percent;
+        alert("Доход в месяц с депозита: " + appData.mothIncome);
+    }
+}
+
+checkSavings();
